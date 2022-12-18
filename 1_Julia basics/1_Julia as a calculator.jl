521a8af23dd3862968722ad6c93b04eb8b6bd3a7
garcarius <edgarcogollo@gmail.com>
2022-12-18 14:37:14 -0500

    introduccion julia

---
+++ 1_Julia basics/1_Julia as a calculator.jl
@@ -0,0 +1,11 @@
+#=
+Writing your code into a script instead of the console means you will have it saved for later. This allows you to rerun the code, modify it and run it with different inputs, or check for any mistakes in the code you used earlier.
+
+Here you are writing a script to keep track of your hours worked throughout the week. You have been adding to it every day to get a total.
+=#
+
+# Add the comment on the line below
+#Hours worked this week
+
+# Add the hours worked
+39+ 37.5 + 40+ 42
